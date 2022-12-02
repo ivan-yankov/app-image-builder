b2fe37c4efbd546c8d136cd82b0aaeba2bc90c76
Ivan <ivan.iankov@gmail.com>
2022-12-02 15:32:37 +0200

    Improvements

--- build-jvm-based-app-image.sh
+++ build-jvm-based-app-image.sh
@@ -1,10 +1,11 @@
 # https://www.booleanworld.com/creating-linux-apps-run-anywhere-appimage
 function build-jvm-based-app-image {
-  is-defined $1 && is-defined $2 && is-defined $3 || return 1
+  is-defined $1 && is-defined $2 && is-defined $3 && is-defined $4 || return 1
 
   ini_file=$1
   resource_dir=$2
   cache_dir=$3
+  app_image=$4
 
   project_dir=$(dirname $ini_file)
 
@@ -17,32 +18,37 @@ function build-jvm-based-app-image {
   jvm_xmx=$(get-ini-value JvmXmx $ini_file)
   application_jars=$(get-ini-value ApplicationJars $ini_file)
   main_class=$(get-ini-value MainClass $ini_file)
-  application_name=$(get-ini-value ApplicationName $ini_file)
   parameters=$(get-ini-value Parameters $ini_file)
+  application_name=$(get-ini-value ApplicationName $ini_file)
   is_terminal_application=$(get-ini-value IsTerminalApplication $ini_file)
   icon_file_ini=$(get-ini-value IconFile $ini_file)
   before=$(get-ini-value Before $ini_file)
   after=$(get-ini-value After $ini_file)
 
   icon_file=$resource_dir/icon.png
-  is-defined $icon_file_ini && icon_file=$project_dir/$icon_file_ini
+  (is-defined $icon_file_ini > /dev/null) && icon_file=$project_dir/$icon_file_ini
 
   rm -rf $app_dir
   mkdir $app_dir
   mkdir $app_dir/jar
 
-  cp $resource_dir/AppRun $app_dir/AppRun
-  sed -i "s/###JVM_XMS###/$jvm_xms/" $app_dir/AppRun
-  sed -i "s/###JVM_XMX###/$jvm_xmx/" $app_dir/AppRun
-  sed -i "s/###MAIN_CLASS###/$main_class/" $app_dir/AppRun
-  sed -i "s/###PARAMETERS###/$parameters/" $app_dir/AppRun
-  sed -i "s/###BEFORE###/$before/" $app_dir/AppRun
-  sed -i "s/###AFTER###/$after/" $app_dir/AppRun
-  
-  cp $resource_dir/template.desktop $app_dir/$application_name.desktop
-  sed -i "s/###APPLICATION_NAME###/$application_name/" $app_dir/$application_name.desktop
-  sed -i "s/###IS_TERMINAL_APPLICATION###/$is_terminal_application/" $app_dir/$application_name.desktop
-  
+  local app_run=$app_dir/AppRun
+  echo '#!/bin/sh' > $app_run
+  echo 'cd "$(dirname "$0")"' >> $app_run
+  echo "$before" >> $app_run
+  echo "./jre/bin/java -Xms$jvm_xms -Xmx$jvm_xmx -Dfile.encoding=UTF-8 -classpath \"jar/*\" $main_class $parameters" '"$@"' >> $app_run
+  echo "$after" >> $app_run
+  sudo chmod +x $app_run
+
+  local desktop=$app_dir/$application_name.desktop
+  echo "[Desktop Entry]" > $desktop
+  echo "Type=Application" >> $desktop
+  echo "Name=$application_name" >> $desktop
+  echo "Icon=icon" >> $desktop
+  echo "Categories=Utility" >> $desktop
+  echo "Terminal=$is_terminal_application" >> $desktop
+  echo "X-AppImage-Version=0.1.09" >> $desktop
+
   cp $icon_file $app_dir/icon.png
   
   cp $project_dir/$application_jars $app_dir/jar
@@ -67,6 +73,6 @@ function build-jvm-based-app-image {
   
   # build app-image
   $cache_dir/appimagetool-$arch.AppImage $app_dir
-  
-  mv $application_name-$arch.AppImage $project_dir/$application_name.AppImage
+
+  sudo mv $project_dir/$application_name-$arch.AppImage $app_image
 }
